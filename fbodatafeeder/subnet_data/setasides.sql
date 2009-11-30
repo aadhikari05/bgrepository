f332d30bcd4274de184e90916cd6d2526aeb4bf5
Puspendu Pal <ppal@Puspendu-Pals-MacBook-Pro-15.local>
2009-11-29 22:43:19 -0500

    added the sql files for setasides and solicitation types

---
+++ fbodatafeeder/subnet_data/setasides.sql
@@ -0,0 +1,17 @@
+insert into setasides (s_type,s_value,created_at,updated_at) values ('Total Small Business','Total Small Business',current_timestamp,current_timestamp);
+insert into setasides (s_type,s_value,created_at,updated_at) values ('Competitive 8(a)','Competitive 8(a)',current_timestamp,current_timestamp);
+insert into setasides (s_type,s_value,created_at,updated_at) values ('HUBZone','HUBZone',current_timestamp,current_timestamp);
+insert into setasides (s_type,s_value,created_at,updated_at) values ('Service-Disabled Veteran-Owned Small Business','Service-Disabled Veteran-Owned Small Business',current_timestamp,current_timestamp);
+insert into setasides (s_type,s_value,created_at,updated_at) values ('Partial Small Business','Partial Small Business',current_timestamp,current_timestamp);
+insert into setasides (s_type,s_value,created_at,updated_at) values ('Total HBCU / MI','Total HBCU / MI',current_timestamp,current_timestamp);
+insert into setasides (s_type,s_value,created_at,updated_at) values ('Emerging Small Business','Emerging Small Business',current_timestamp,current_timestamp);
+insert into setasides (s_type,s_value,created_at,updated_at) values ('Veteran-Owned Small Business','Veteran-Owned Small Business',current_timestamp,current_timestamp);
+insert into setasides (s_type,s_value,created_at,updated_at) values ('Partial HUB-Zone','Partial HUB-Zone',current_timestamp,current_timestamp);
+insert into setasides (s_type,s_value,created_at,updated_at) values ('Total Small Disadvantage Business','Total Small Disadvantage Business',current_timestamp,current_timestamp);
+insert into setasides (s_type,s_value,created_at,updated_at) values ('Very Small Business','Very Small Business',current_timestamp,current_timestamp);
+insert into setasides (s_type,s_value,created_at,updated_at) values ('Partial HBCU / MI','Partial HBCU / MI',current_timestamp,current_timestamp);
+insert into setasides (s_type,s_value,created_at,updated_at) values ('Disadvantaged Business Enterprise','Disadvantaged Business Enterprise',current_timestamp,current_timestamp);
+insert into setasides (s_type,s_value,created_at,updated_at) values ('Women Owned Small Business','Women Owned Small Business',current_timestamp,current_timestamp);
+
+	
+	
---
+++ fbodatafeeder/subnet_data/solicitaion_types.sql
@@ -0,0 +1,7 @@
+insert into solicitation_types (s_type,s_value,created_at,updated_at) values ('Presolicitation','PRESOL',current_timestamp,current_timestamp);
+insert into solicitation_types (s_type,s_value,created_at,updated_at) values ('Combined Solicitation','COMBINE',current_timestamp,current_timestamp);
+insert into solicitation_types (s_type,s_value,created_at,updated_at) values ('Sources Sought Notice','SRCSGT',current_timestamp,current_timestamp);
+insert into solicitation_types (s_type,s_value,created_at,updated_at) values ('Special Notice','SNOTE',current_timestamp,current_timestamp);
+insert into solicitation_types (s_type,s_value,created_at,updated_at) values ('Modifications','MOD',current_timestamp,current_timestamp);
+insert into solicitation_types (s_type,s_value,created_at,updated_at) values ('Amendments','AMDCSS',current_timestamp,current_timestamp);
+insert into solicitation_types (s_type,s_value,created_at,updated_at) values ('Subcontract Solicitation','SUBSOL',current_timestamp,current_timestamp);
